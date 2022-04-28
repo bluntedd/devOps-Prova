637e19445199b807fda669ee43aa68d6ec663d06
bluntedd <102332038+bluntedd@users.noreply.github.com>
2022-04-28 10:26:24 -0300

    Add files via upload

---
+++ index.test.js
@@ -0,0 +1,59 @@
+
+const index = require("./index");
+
+test("Soma de dois números", () =>  {
+
+const result = index.add(3,5);
+
+expect(result).toEqual(8);
+
+}     
+
+);
+
+
+test("subtracao de dois números", () =>
+
+{
+
+    const result = index.subtract(10,2);
+    expect(result).toEqual(8);
+},
+
+
+
+test("divisao de dois números", () =>
+
+{
+
+    const result = index.division(10,2);
+    expect(result).toEqual(5);
+},
+
+
+test("multiplicaçao de dois números", () =>
+
+{
+
+    const result = index.multiplication(10,2);
+    expect(result).toEqual(5);
+}
+
+
+)
+))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
